55619ffdd3aa111e0cfd73043ecc4553391cb875
Isaac Zavala <isaac.zavala@ibm.com>
2020-07-31 15:02:03 -0500

    Update command to not be sudo

--- modules/vmware_ign_config/scripts/config_firewall.sh
+++ modules/vmware_ign_config/scripts/config_firewall.sh
@@ -82,6 +82,6 @@ function configureFirewall() {
         sudo firewall-cmd --reload
     fi
 }
-PRIVATE_INT=$(sudo ifconfig | grep -B1 "${PRIVATE_IP}" | awk '$1!="inet" && $1!="--" {print $1}'| cut -d':' -f1)
+PRIVATE_INT=$(ifconfig | grep -B1 "${PRIVATE_IP}" | awk '$1!="inet" && $1!="--" {print $1}'| cut -d':' -f1)
 identifyPlatform
 configureFirewall
