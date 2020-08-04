dd31c103ef631885746624d003f7dcb24b9aa50f
Isaac Zavala <isaac.zavala@ibm.com>
2020-08-04 14:23:43 -0500

    Remove sudo as it is ran as root

--- modules/vmware_ign_config/scripts/get_interfaces.sh
+++ modules/vmware_ign_config/scripts/get_interfaces.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 PRIVATE_IP=$1
-PRIVATE_INT=$(sudo ifconfig | grep -B1 "${PRIVATE_IP}" | awk '$1!="inet" && $1!="--" {print $1}'| cut -d':' -f1)
+PRIVATE_INT=$(ifconfig | grep -B1 "${PRIVATE_IP}" | awk '$1!="inet" && $1!="--" {print $1}'| cut -d':' -f1)
 echo {\"privateintf\": \"${PRIVATE_INT}\"}
